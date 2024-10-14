b2a74d1fe8ff5238f5738c3a01863442d1ba838c
Nicolas <nicolasdelfino585@gmail.com>
2024-10-14 14:54:00 -0300

    solucion 06.2 done

---
+++ arrays_06_2.js
@@ -0,0 +1,23 @@
+/* */
+
+// solucion curso
+let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
+
+let lastPos = mySampleArray.length - 1;
+let stopAt = 0;
+
+for(let i = lastPos; i >= stopAt ; i = i - 1)
+{
+	console.log(mySampleArray[i]);
+}
+
+//  mi solución
+
+let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
+
+
+for(let i = mySampleArray.length -1; i >= 0; i = i - 1)
+{
+
+	console.log(mySampleArray[i]);
+}
